3c20207921b20135a66cb94e0336885fb78b6460
Sandali05 <sandalikaru123@gmail.com>
2024-10-08 18:26:23 +0530

    final resize

--- frontend/src/views/admin/delivery/DeliveryScreen.tsx
+++ frontend/src/views/admin/delivery/DeliveryScreen.tsx
@@ -268,7 +268,7 @@ const DeliveryScreen = () => {
                     <img
                       src={`/drivers/${delivery.image}`}
                       // alt={delivery.name}
-                      style={{ width: "90px", height: "70px", borderRadius: "10%" }}
+                      style={{ width: "200px", height: "100px", borderRadius: "10%" }}
                     />
                   </TableCell>
                   <TableCell
