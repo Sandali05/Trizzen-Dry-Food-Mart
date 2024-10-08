3ecfeca5fcd2522fe7414134eabbc44e964328cc
Sandali05 <sandalikaru123@gmail.com>
2024-10-08 18:18:57 +0530

    image boarder

--- frontend/src/views/admin/delivery/DeliveryScreen.tsx
+++ frontend/src/views/admin/delivery/DeliveryScreen.tsx
@@ -268,7 +268,7 @@ const DeliveryScreen = () => {
                     <img
                       src={`/drivers/${delivery.image}`}
                       // alt={delivery.name}
-                      style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+                      style={{ width: "0px", height: "50px", borderRadius: "10%" }}
                     />
                   </TableCell>
                   <TableCell
